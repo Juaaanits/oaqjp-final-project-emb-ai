c2314bfc13183f91688708af964875cdef15de5d
Juaaanits <juanitoramos113@gmail.com>
2025-07-05 06:42:04 -0400

    Packaged the application

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+        "Content-Type": "application/json",
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    input_json = { "raw_document": { "text": text_to_analyze } }
+
+    # Sending POST request to Watson NLP
+    response = requests.post(url, headers=headers, json=input_json)
+
+    if response.status_code != 200:
+        return {"error": f"Request failed with status code {response.status_code}"}
+
+    #Converting response text to a dictionary
+    response_dict = json.loads(response.text)
+
+    #Extracting emotion scores
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+    
+    #Determining the dominant emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    # Constructing final dictionary
+    result = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
+
+    return result
